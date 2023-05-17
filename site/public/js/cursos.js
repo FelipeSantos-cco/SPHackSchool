00dae70f68c054670cae74becf928ce09ffb86a4
unknown <felipe.almeida@sptech.school>
2023-05-16 22:57:26 -0300

    Teste 01 OK
    testes no console

--- site/public/js/cursos.js
+++ site/public/js/cursos.js
@@ -5,5 +5,10 @@ var vt_curso_texto = ['Aqui você vai aprender tudo relacionado a lógica'];
 var vt_curso_link = ['https://www.google.com'];
 
 for(var i = 0; i < vt_curso_titulo.length; i++ ){
-    console.log(`Teste`);
+    // console.log(`Nome do Curso: ${vt_curso_titulo[i]}`);
+    // console.log(`Endereço da imagem: ${vt_curso_Enderecoimg[i]}`);
+    // console.log(`Texto: ${vt_curso_texto[i]}`);
+    // console.log(`Link da pág: ${vt_curso_link[i]}`);
+
+
 }
