3e92f30a81ed9fd899816860e04d3d565a36d24a
unknown <felipe.almeida@sptech.school>
2023-05-16 22:52:16 -0300

    Teste cursos.js

--- site/public/js/cursos.js
+++ site/public/js/cursos.js
@@ -1,8 +1,9 @@
 
-var vt_curso_titulo = [];
-var vt_curso_Enderecoimg = [];
-var vt_curso_texto = [];
-var vt_curso_link = [];
+var vt_curso_titulo = ['Curso de lógica'];
+var vt_curso_Enderecoimg = ['./assets/img/teste.png'];
+var vt_curso_texto = ['Aqui você vai aprender tudo relacionado a lógica'];
+var vt_curso_link = ['https://www.google.com'];
 
-
-
+for(var i = 0; i < vt_curso_titulo.length; i++ ){
+    console.log(`Teste`);
+}
