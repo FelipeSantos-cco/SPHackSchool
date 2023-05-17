e29493e28e57e66262ae0b5dd8bae86dfab5149d
unknown <felipe.almeida@sptech.school>
2023-05-16 23:09:21 -0300

    OK OK OK - cursos em vetores
    A implementação de cursos em vetores no html funcinou

--- site/public/js/cursos.js
+++ site/public/js/cursos.js
@@ -1,6 +1,6 @@
 
 var vt_curso_titulo = ['Curso de lógica'];
-var vt_curso_Enderecoimg = ['./assets/img/teste.png'];
+var vt_curso_Enderecoimg = ['./assets/img/protecao.png'];
 var vt_curso_texto = ['Aqui você vai aprender tudo relacionado a lógica'];
 var vt_curso_link = ['https://www.google.com'];
 
@@ -10,5 +10,13 @@ for(var i = 0; i < vt_curso_titulo.length; i++ ){
     // console.log(`Texto: ${vt_curso_texto[i]}`);
     // console.log(`Link da pág: ${vt_curso_link[i]}`);
 
+    div_addCursos.innerHTML += `
+    <div class="card">
+        <img src="${vt_curso_Enderecoimg[i]}">
+        <h2>${vt_curso_titulo[i]}</h2>
+        <p>${vt_curso_texto[i]}</p>
+        <p><a href="${vt_curso_link[i]}">Clique aqui para saber mais!!</a></p>
+    </div>
+    `;
 
 }
