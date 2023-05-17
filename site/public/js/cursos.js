deecbcd304567a8c709c8d8c888c7777b1602d95
unknown <felipe.almeida@sptech.school>
2023-05-16 23:15:38 -0300

    Refatorando cursos.js
    Adicionando JSON com vetores em vez de só vetores

--- site/public/js/cursos.js
+++ site/public/js/cursos.js
@@ -1,10 +1,25 @@
 
-var vt_curso_titulo = ['Curso de lógica'];
-var vt_curso_Enderecoimg = ['./assets/img/protecao.png'];
-var vt_curso_texto = ['Aqui você vai aprender tudo relacionado a lógica'];
-var vt_curso_link = ['https://www.google.com'];
+/*
+    Script responsavél por "alimentar" a página
 
-for(var i = 0; i < vt_curso_titulo.length; i++ ){
+*/
+
+
+// var vt_curso_titulo = ['Curso de lógica'];
+// var vt_curso_Enderecoimg = ['./assets/img/protecao.png'];
+// var vt_curso_texto = ['Aqui você vai aprender tudo relacionado a lógica'];
+// var vt_curso_link = ['https://www.google.com'];
+
+var curso = {
+    titulo: ['Curso de lógica'],
+    enderecoImg: ['./assets/img/protecao.png'],
+    texto: ['Aqui você vai aprender tudo relacionado a lógica'],
+    link: ['https://www.google.com']
+}
+
+
+
+for(var i = 0; i < curso.titulo.length; i++ ){
     // console.log(`Nome do Curso: ${vt_curso_titulo[i]}`);
     // console.log(`Endereço da imagem: ${vt_curso_Enderecoimg[i]}`);
     // console.log(`Texto: ${vt_curso_texto[i]}`);
@@ -12,10 +27,10 @@ for(var i = 0; i < vt_curso_titulo.length; i++ ){
 
     div_addCursos.innerHTML += `
     <div class="card">
-        <img src="${vt_curso_Enderecoimg[i]}">
-        <h2>${vt_curso_titulo[i]}</h2>
-        <p>${vt_curso_texto[i]}</p>
-        <p><a href="${vt_curso_link[i]}">Clique aqui para saber mais!!</a></p>
+        <img src="${curso.enderecoImg[i]}">
+        <h2>${curso.titulo[i]}</h2>
+        <p>${curso.texto[i]}</p>
+        <p><a href="${curso.link[i]}">Clique aqui para saber mais!!</a></p>
     </div>
     `;
 
