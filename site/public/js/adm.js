1520ec43c8ad6a8f9a7116861d0d63750eaaf211
FelipeSantos-cco <felipe.almeida@sptech.school>
2023-06-05 12:56:45 -0300

    Ajustes finais anstes da apresentação :)

--- site/public/js/adm.js
+++ site/public/js/adm.js
@@ -86,11 +86,69 @@ function topCursos(){
         console.error(resposta);
 
     });
+    
+}
+
+    
+    
+function grafico(){
+    fetch("/admin/topFavoritos").then(function (resposta) {
+
+        if (resposta.ok) {
+            resposta.json().then(function (resposta) {
+                const ctx = document.getElementById('myChart');
+                
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                      labels: [resposta[0].nomeCurso, resposta[1].nomeCurso, resposta[2].nomeCurso],
+                      datasets: [{
+                        label: '# of Votes',
+                        data: [resposta[0].qtdFav, resposta[1].qtdFav, resposta[2].qtdFav],
+                        borderWidth: 1
+                      }]
+                    },
+                    options: {
+                      scales: {
+                        y: {
+                          beginAtZero: true
+                        }
+                      }
+                    }
+                  });
 
 
+                // for(let i = 0; i < resposta.length; i++){
+                //     var curso = resposta[i];
 
+                //     var img = curso.urlImgCurso;
 
+                //     if (img == '-1') {
+                //         img = '../assets/sph-gif.gif'
+                //     }
 
+                //     div_resp.innerHTML += `
+                //         <div class="card" >
+                //             <p><b>${i+1}° Lugar</b></p>
+                //             <p>${curso.qtdFav} Favoritos</p>
+                //             <img src="${img}">
+                //             <h3>${curso.nomeCurso}</h3>
+                //             <button onclick="curso.urlCurso" class="ir"> 
+                //                 <a href="${curso.urlCurso}" target="_blank">
+                //                     Acesse o curso clicando aqui
+                //                 </a>
+                //             </button>
+                //         </div>`
+                // }
+            
+            });
+        } else {
+            console.log('Erro no Adm - Top Cursos');
+        }
+    }).catch(function (resposta) {
+        console.error(resposta);
+
+    });
 }
 
 function geral(){
@@ -98,4 +156,5 @@ function geral(){
     totalPlat();
     totalUser();
     topCursos();
+    grafico();
 }
